b72ac7d3a4b64c074acb39b04413c95459db9461
tjdud9329 <tjdud9329@gmail.com>
2026-08-22 17:33:25 +0900

    refactor: app.tsx 내  사용하지 않는 변수 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import useAuthStore from './stores/useAuthStore';
-import { useEffect } from 'react';
 
 import LoginPage from '../src/pages/LoginPage';
 import SignupPage from '../src/pages/SingupPage';
 
 function App() {
-  const accessToken = useAuthStore((state) => state.accessToken);
   return (
     <BrowserRouter>
       <Routes>
