b0bf3075f32992af4e323f6412ed7427c8bc2a55
tjdud9329 <tjdud9329@gmail.com>
2026-08-26 14:58:27 +0900

    feat: 로그인/회원가입 페이지 베이스 로직 작성 및 색상추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,39 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../api/authAPI';
+import useAuthStore from '../stores/useAuthStore';
+
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const setAuth = useAuthStore((state) => state.setAuth);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //로딩 여부
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    //전송중이거나 이메일/패스워드가 없다면 리턴
+    if (!email || !password || isLoading) return;
+
+    try {
+      setIsLoading(true);
+
+      const data = await login({ email, password });
+
+      //백엔드에서 준 data 내 토큰을 zustand 스토어에 저장
+      setAuth(data.accessToken, data.refreshToken, '');
+
+      navigate('/');
+    } catch (error) {
+      alert('로그인에 실패하였습니다. 이메일과 비밀번호를 확인해주세요.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return <div>LoginPage</div>;
 };
 
--- src/pages/SingupPage.tsx
+++ src/pages/SingupPage.tsx
@@ -1,4 +1,35 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signup } from '../api/authAPI';
+import useAuthStore from '../stores/useAuthStore';
+
 const SingupPage = () => {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!name || !password || !email || isLoading) return;
+
+    try {
+      setIsLoading(true);
+      await signup({ name, email, password });
+
+      alert('회원가입이 완료되었습니다! 로그인 후 이용해주세요.');
+      navigate('/login');
+    } catch (error) {
+      alert('회원가입에 실패하였습니다. 다시 시도해주세요.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return <div>SingupPage</div>;
 };
 
